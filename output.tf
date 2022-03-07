8e17ccd01c6311ccb880e53b80bc4b28e359aa65
Ola-Daniel <doola-olorun@student.oauife.edu.ng>
2022-03-07 09:21:36 +0800

    added rds.tf

---
+++ output.tf
@@ -0,0 +1,36 @@
+output "db_instance_address" {
+  description = "The address of the RDS instance"
+  value       = module.db.db_instance_address
+}
+
+output "db_instance_endpoint" {
+  description = "The connection endpoint"
+  value       = module.db.db_instance_endpoint
+}
+
+output "db_instance_id" {
+  description = "The RDS instance ID"
+  value       = module.db.db_instance_id
+}
+
+output "db_instance_status" {
+  description = "The RDS instance status"
+  value       = module.db.db_instance_status
+}
+
+output "db_instance_name" {
+  description = "The database name"
+  value       = module.db.db_instance_name
+}
+
+output "db_instance_username" {
+  description = "The master username for the database"
+  value       = module.db.db_instance_username
+  sensitive   = true
+}
+
+output "db_instance_password" {
+  description = "The database password (this password may be old, because Terraform doesn't track it after initial creation)"
+  value       = module.db.db_instance_password
+  sensitive   = true
+}
---
+++ rds.tf
@@ -0,0 +1,80 @@
+# rds.tf
+module "db" {
+  source  = "terraform-aws-modules/rds/aws"
+
+  identifier = "prestashop"
+
+  engine            = "mysql"
+  engine_version    = "5.7.25"
+  instance_class    = "db.t2.micro"
+  allocated_storage = 5
+
+  db_name  = "prestashop"
+  username = "prestashop_user"
+  port     = "3306"
+
+  iam_database_authentication_enabled = true
+
+  vpc_security_group_ids = [
+    aws_security_group.ingress_database.id,
+    aws_security_group.egress_all.id,
+  ]
+
+  maintenance_window = "Mon:00:00-Mon:03:00"
+  backup_window      = "03:00-06:00"
+
+  # Enhanced Monitoring - see example for details on how to create the role
+  # by yourself, in case you don't want to create it automatically
+  monitoring_interval = "30"
+  monitoring_role_name = "MyRDSMonitoringRole"
+  create_monitoring_role = true
+
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
+
+  # DB subnet group
+  create_db_subnet_group = false
+  subnet_ids             = [
+    aws_subnet.public_a.id,
+    aws_subnet.public_b.id,
+  ]
+
+  # DB parameter group
+  family = "mysql5.7"
+
+  # DB option group
+  major_engine_version = "5.7"
+
+  # Database Deletion Protection
+  deletion_protection = true
+
+  parameters = [
+    {
+      name = "character_set_client"
+      value = "utf8mb4"
+    },
+    {
+      name = "character_set_server"
+      value = "utf8mb4"
+    }
+  ]
+
+  options = [
+    {
+      option_name = "MARIADB_AUDIT_PLUGIN"
+
+      option_settings = [
+        {
+          name  = "SERVER_AUDIT_EVENTS"
+          value = "CONNECT"
+        },
+        {
+          name  = "SERVER_AUDIT_FILE_ROTATIONS"
+          value = "37"
+        },
+      ]
+    },
+  ]
+}
